e89bb53b2e8da79f0d3c0c35a45ab065fbb2d21b
iambouamar <iambouamar@gmail.com>
2017-11-11 16:45:29 +0000

    GeoSchema ajouté

--- models/prof.js
+++ models/prof.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
 const ProfSchema = new Schema({
     nom: {
         type: String,
@@ -12,7 +24,8 @@ const ProfSchema = new Schema({
     disponible: {
         type: Boolean,
         default: false
-    }
+    },
+    geometry: GeoSchema
 }); 
 
 const Prof = mongoose.model('prof', ProfSchema);
